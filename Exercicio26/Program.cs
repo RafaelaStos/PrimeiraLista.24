0a8be4837b83232a39f0255a466d16435d5d00f4
Rafaela dos Santos Morais <morais.rafa508@gmail.com>
2024-03-19 14:34:25 -0300

    organizaçao dos codigos em pastas

---
+++ Exercicio26/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercicio26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int resultdao = 1;
+
+            Console.WriteLine("Digite um número");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write(resultdao + ", ");
+            for (int i = 0; i < a; i++)
+            {
+                resultdao = resultdao + i ;
+                Console.Write(resultdao + ", ");
+               
+            }
+        }
+    }
+}
---
+++ Exercicio28/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+
+namespace Exercicio28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    int resultado = i * j;
+                    Console.Write("{0} x {1} = {2}\n", i, j, resultado);
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
